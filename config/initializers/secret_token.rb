e989af8f5c55e87702ef89d1cc29be4cfb648818
Matthew Burnett <matt@sowhatsthdeal.com>
2012-01-26 01:08:33 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coda::Application.config.secret_token = 'fcdca647677ad0f1baa013fea1f99028c76af9b7e25e35b0dbb1b58d8547d81ecd52c978a8bad83413c678e504acc9be59199581c6889a3fa841c6e55da02646'
