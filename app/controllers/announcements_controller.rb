598f06cb9d259d3c024630731f4fc526b33d6bcd
Matthew Burnett <matt@sowhatsthdeal.com>
2012-11-03 10:02:33 -0400

    reversed announcements order, also trying asset pipeline stuff locally

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -2,7 +2,7 @@ class AnnouncementsController < ApplicationController
   # GET /announcements
   # GET /announcements.json
   def index
-    @announcements = Announcement.all
+    @announcements = Announcement.all.reverse
 
     respond_to do |format|
       format.html # index.html.erb
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,5 @@ Coda::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  config.assets.prefix = "/dev-assets"
 end
