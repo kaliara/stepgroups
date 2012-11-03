d5db2f4b21e8dc430ef14e219175850857184c6b
Matthew Burnett <matt@sowhatsthdeal.com>
2012-11-03 10:06:50 -0400

    adding new assets

---
+++ public/assets/ckeditor/config-3a3bff2faf2ecb31896d1e852f546c44.js
@@ -0,0 +1,5 @@
+/*
+Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+CKEDITOR.editorConfig=function(e){e.toolbar="Custom",e.toolbar_Admin=[["Styles","Bold","Italic","Underline","TextColor","Format","Font","FontSize","NumberedList","BulletedList","JustifyLeft","JustifyCenter","JustifyRight","Blockquote","-","Link","Unlink","-","Image","-","RemoveFormat","Source"]],e.toolbar_Custom=[["Blockquote","Bold","Italic","Underline","TextColor","FontSize","NumberedList","BulletedList","-","Link","Unlink"]],e.toolbarCanCollapse=!1,e.bodyClass="ckeditor",e.disableNativeSpellChecker=!1};
