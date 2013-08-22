f61624f110777d48c6f1e368c0f1a8e4192a8c7e
Matthew Kaliara <matthew@patentspark.com>
2013-08-22 18:02:58 -0400

    updated phone messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ActiveRecord::Base
   attr_accessor :antispam_value
   
-  validates_presence_of :contact_details, :on => :create, :message => "must be entered"
+  # validates_presence_of :contact_details, :on => :create, :message => "must be entered"
+  validates_presence_of :body, :on => :create, :message => " message can't be blank"
   validates_captcha_with :antispam_value, :on => :create, :message => "must be answered correctly"
 end
