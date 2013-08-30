9ea2567eb25a660214b241c8a44a5d43e95b10dd
Matthew Kaliara <matthew@patentspark.com>
2013-08-30 01:30:33 -0400

    asset precompile

---
+++ public/assets/ckeditor/plugins/styles/styles/default-6662b8587ce497a8cb4e86d47b9ab015.js
@@ -0,0 +1,5 @@
+/*
+Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+CKEDITOR.stylesSet.add("default",[{name:"Header 2",element:"h2"},{name:"Header 3",element:"h3"},{name:"Header 4",element:"h4"},{name:"Header 5",element:"h5"},{name:"Inline Quotation",element:"q"}]);
